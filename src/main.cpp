c30e6de70f1f0a005e48c6983ebe665af54cf779
GDM1705 <71228914+GDM1705@users.noreply.github.com>
2021-06-02 16:09:17 +0300

    last

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,12 @@
 //  Copyright  2021  Golovanov
-
 #include <iostream>
-#include "TimedDoor.h"
+
+#include "../include/TimedDoor.h"
 
 int main() {
-    TimedDoor test_door(10);
-    test_door.lock();
-    test_door.unlock();
+  TimedDoor tDoor(5);
+  tDoor.lock();
+  tDoor.unlock();
+
+  return 0;
 }
